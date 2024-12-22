59d9b6323546e531f15f7dbcbdfc07c0d7d97d00
unknown <nnope1012@gmail.com>
2024-12-22 11:48:38 +0530

    final commit

---
+++ backend/frontend/src/context/useGetSocketMessage.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+import { useSocketContext } from "./SocketContext.jsx";
+import useConversation from "../statemanage/useConversation.js";
+const useGetSocketMessage = () => {
+  const { socket } = useSocketContext();
+  const { messages, setMessage } = useConversation();
+
+  useEffect(() => {
+    socket.on("newMessage", (newMessage) => {
+      const notification = new Audio(sound);
+      notification.play();
+      setMessage([...messages, newMessage]);
+    });
+    return () => {
+      socket.off("newMessage");
+    };
+  }, [socket, messages, setMessage]);
+};
+export default useGetSocketMessage;
